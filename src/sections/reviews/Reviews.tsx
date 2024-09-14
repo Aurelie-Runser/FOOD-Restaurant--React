8d73ef0f9363d22582a62d07766debc89d5db53e
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-09-14 14:48:42 +0200

    ajout du système de carrousel pour faire défiler les avis

--- src/sections/reviews/Reviews.tsx
+++ src/sections/reviews/Reviews.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 import ReviewCard from '../../components/reviewCard/reviewCard'
 import Title from '../../components/title/Title'
 import Icon from '../../components/icons/Icon';
@@ -6,24 +8,57 @@ import './Reviews.css'
 
 export default function Reviews() {
 
+  const reviews = [
+    {
+      name: 'Willians Jhone',
+      job: 'CEO & Co-Founder',
+      pp: '/img/WilliansJhone.jpg',
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt vel, maxime sit libero incidunt amet atque quos hic accusamus sapiente."
+    },
+    {
+      name: 'Pauline Dubois',
+      job: 'Manager',
+      pp: 'https://cataas.com/cat',
+      text: "Cupcake ipsum dolor sit amet powder. Bonbon ice cream cupcake pie cake wafer danish cupcake lollipop. Tiramisu cake I love I love macaroon tiramisu tiramisu."
+    },
+    {
+      name: 'Lisa Muller',
+      job: 'Student',
+      pp: 'https://placewaifu.com/image/50',
+      text: "Protein, iron, and calcium are some of the nutritional benefits associated with cheeseburgers. Salad is essentially food for rabbits, so don't bother wasting your time."
+    }
+  ]
+
+  const [reviewNum, setReviewNum] = useState(0)
+
+  const reviewPrevious = () => {
+    if(reviewNum != 0) setReviewNum(reviewNum - 1)
+    else setReviewNum(reviews.length-1)
+  }
+
+  const reviewNext = () => {
+    if(reviewNum < reviews.length-1) setReviewNum(reviewNum + 1)
+    else setReviewNum(0)
+  }
+
   return (
     <section className='reviews'>
       <Title subTitle='Testimonials'>Our happy client says</Title>
       
       <div className='reviews--content'>
         <div className='reviewsCarrousel'>
-          <div className='reviewsCarrousel--arrowLeft'>
+          <div className='reviewsCarrousel--arrowLeft' onClick={reviewPrevious}>
             <Icon name="fleche" size="medium" gauche color="orangeWhite"/>
           </div>
 
           <ReviewCard
-            name='Willians Jhone'
-            job='CEO & Co-Founder'
-            pp='/img/WilliansJhone.jpg'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt vel, maxime sit libero incidunt amet atque quos hic accusamus sapiente odio sunt aliquid laudantium nemo, minus, voluptate iste quod at?
+            name={reviews[reviewNum].name}
+            job={reviews[reviewNum].job}
+            pp={reviews[reviewNum].pp}>
+              {reviews[reviewNum].text}
           </ReviewCard>
 
-          <div className='reviewsCarrousel--arrowRight'>
+          <div className='reviewsCarrousel--arrowRight' onClick={reviewNext}>
             <Icon name="fleche" size="medium" color="orangeWhite"/>
           </div>
         </div>

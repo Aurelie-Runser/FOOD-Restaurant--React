9700181c26dff6fa2f47e1bb9688a6d71c075c91
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-09-19 17:46:05 +0200

    ajout du lien et de l'icon dans le bouton

--- src/components/bouton/Bouton.tsx
+++ src/components/bouton/Bouton.tsx
@@ -6,23 +6,27 @@ interface Bouton {
     children: ReactNode,
     size: string | undefined,
     rounded: boolean | undefined
+    link: string | undefined
+    icon: boolean | undefined
 }
 
-export default function Bouton({children, size, rounded}:Bouton) {
+export default function Bouton({children, size, rounded, link, icon}:Bouton) {
     let boutonClass = 'bouton'
 
     if(rounded) boutonClass += ' bouton-rounded'
-
+    
     if(size == 'small') boutonClass += ' bouton-small p-small'
     else if(size == 'big') boutonClass += ' bouton-big h5'
 
-    return (
-        <a href="#">
-            <button className={boutonClass} >
-                <span>
-                    {children}
-                </span>
-            </button>
+    if(icon) boutonClass += ' bouton-icon'
+    
+    return link ? (
+        <a href={link} className={boutonClass}>
+            {children}
         </a>
+    ) : (
+        <button className={boutonClass} >
+            {children}
+        </button>
     )
 }

32600025b47126fb117e4963cabf4138938b0c81
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-09-20 21:02:26 +0200

    recupération de l'email pour pouvoir plus tard intégrer la notif de succes

--- src/components/emailInput/EmailInput.tsx
+++ src/components/emailInput/EmailInput.tsx
@@ -1,12 +1,19 @@
 import './EmailInput.css'
 
 export default function EmailInput({size}:{size: string|undefined}) {
+
+    const formSubmit = (e:any)=>{
+        e.preventDefault()
+        const formulaire = new FormData(e.target)
+        const email = formulaire.get("subscriber")
+        console.log(email)
+    }
     
     return size == 'big' ? (
-        <div className='emailInput-big'>
-            <input type="email" placeholder='Enter your email...' className='emailInput-big--input p'/>
+        <form onSubmit={formSubmit} className='emailInput-big'>
+            <input type="email" name="subscriber" placeholder='Enter your email...' required className='emailInput-big--input p'/>
                 <button className='emailInput-big--button h5'>Subscribe</button>
-        </div>
+        </form>
     ) : (
         <div>
             hello

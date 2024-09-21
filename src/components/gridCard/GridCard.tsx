a17c9b3675f5f62717f5c53eb4d7ecd75f94fb67
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-09-21 10:17:22 +0200

    ajustement valeur du key du list.map de la grille des recettes

--- src/components/gridCard/GridCard.tsx
+++ src/components/gridCard/GridCard.tsx
@@ -119,16 +119,16 @@ import './GridCard.css'
 export default function GridCard({num}:{ num: number|undefined}) {
 
   const { loading, data, errors } = useFetch();
-  console.log(loading)
-  console.log(data)
-  console.log(errors)
+  // console.log(loading)
+  // console.log(data)
+  // console.log(errors)
 
   return ( <>
     {loading && <svg style={{display: 'block', margin: 'auto'}} xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 24 24"><circle cx="4" cy="12" r="3" fill="#F48E28"><animate id="svgSpinners3DotsBounce0" attributeName="cy" begin="0;svgSpinners3DotsBounce1.end+0.25s" calcMode="spline" dur="0.6s" keySplines=".33,.66,.66,1;.33,0,.66,.33" values="12;6;12"/></circle><circle cx="12" cy="12" r="3" fill="#F48E28"><animate attributeName="cy" begin="svgSpinners3DotsBounce0.begin+0.1s" calcMode="spline" dur="0.6s" keySplines=".33,.66,.66,1;.33,0,.66,.33" values="12;6;12"/></circle><circle cx="20" cy="12" r="3" fill="#F48E28"><animate id="svgSpinners3DotsBounce1" attributeName="cy" begin="svgSpinners3DotsBounce0.begin+0.2s" calcMode="spline" dur="0.6s" keySplines=".33,.66,.66,1;.33,0,.66,.33" values="12;6;12"/></circle></svg>}
     { data &&
         <ul className='gridCard gridCard--product'>
         {data.map((p) => (
-            <li key={p.img}>
+            <li key={p.title}>
                 <ProductCard 
                     name={p.title}
                     img={p.imgSrc}
@@ -139,5 +139,6 @@ export default function GridCard({num}:{ num: number|undefined}) {
         )).slice(0, num)}
         </ul>
     }
+    { errors && <p>Erreur lors de la récupération des recettes</p>}
   </>)
 }

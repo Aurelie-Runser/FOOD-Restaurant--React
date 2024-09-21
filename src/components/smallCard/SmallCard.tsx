ad012e3fc7fb436f0c2606702d242d97b44d4499
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-09-21 16:19:31 +0200

    creation des cards pour le hero

---
+++ src/components/smallCard/SmallCard.tsx
@@ -0,0 +1,35 @@
+import Icon from '../icons/Icon';
+import Bouton from '../bouton/Bouton';
+
+import './SmallCard.css'
+
+interface SmallCard {
+    title: string,
+    img: string,
+    text: string,
+    price: number,
+}
+
+export default function SmallCard({title, img, text, price}:SmallCard) {
+  return (
+    <div className="smallCard">
+        <div className="smallCard--bg">
+            <div className="smallCard--content">
+                <h4>{title}</h4>
+
+                <p className="smallCard--text">{text}</p>
+                <p className="h5 smallCard--price">{price}</p>
+            </div>
+
+            <div className='smallCard--buttons'>
+                <Icon name="fleche" size="medium" color="black" />
+                <Icon name="fleche" size="medium" color="white" gauche />
+            </div>
+        </div>
+
+        <div className="smallCard--img">
+            <img src={'/img/'+img+'.png'} alt={"photo de "+title} />
+        </div>
+    </div>
+  )
+}

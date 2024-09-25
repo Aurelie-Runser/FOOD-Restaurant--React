c817741c8cc2ffb6d14017f56618b1f72a7fdcab
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-09-25 17:38:45 +0200

    création du router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,54 @@
 // import { useState } from 'react'
+import { createBrowserRouter, Outlet, RouterProvider, useNavigation } from 'react-router-dom'
 import Header from './sections/header/Header'
 import Home from './pages/Home'
+import Recettes from './pages/Recettes'
+import Recette from './pages/Recette'
 import Footer from './sections/footer/Footer'
+import Error from './pages/Error'
 
 import './main.css'
+import Chargement from './components/Chargement'
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    errorElement: <Error/>,
+    children: [
+      {
+        path: '',
+        element: <Home/>
+      },
+      {
+        path: 'recettes',
+        children: [
+          {
+            path: '',
+            element: <Recettes/>,
+          },
+          {
+            path: ':id',
+            element: <Recette/>,
+          }
+        ]
+      }
+    ]
+  },
+])
 
+function Layout(){
+  const {state} = useNavigation()
   return <>
     <Header/>
-    <Home/>
+    <main>
+      {state != 'loading' && <Outlet/>}
+      {state == 'loading' && <Chargement/>}
+    </main>
     <Footer/>
   </>
 }
 
-export default App
+export default function App() {
+  return <RouterProvider router={router}/>
+}

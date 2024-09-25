5619f0d6f424fb8612092d6e7d78218fb27e45a9
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-09-25 17:37:51 +0200

    creation du composant chargement

--- src/components/gridCard/GridCard.tsx
+++ src/components/gridCard/GridCard.tsx
@@ -6,6 +6,7 @@ import Bouton from '../../components/bouton/Bouton'
 import Icon from '../../components/icons/Icon'
 
 import './GridCard.css'
+import Chargement from '../Chargement'
 
 // const listProduct = [
 //     {
@@ -146,7 +147,7 @@ export default function GridCard({num}:{ num?: number}) {
   }, [numProduct])
 
   return ( <>
-    {loading && <svg style={{display: 'block', margin: 'auto'}} xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 24 24"><circle cx="4" cy="12" r="3" fill="#F48E28"><animate id="svgSpinners3DotsBounce0" attributeName="cy" begin="0;svgSpinners3DotsBounce1.end+0.25s" calcMode="spline" dur="0.6s" keySplines=".33,.66,.66,1;.33,0,.66,.33" values="12;6;12"/></circle><circle cx="12" cy="12" r="3" fill="#F48E28"><animate attributeName="cy" begin="svgSpinners3DotsBounce0.begin+0.1s" calcMode="spline" dur="0.6s" keySplines=".33,.66,.66,1;.33,0,.66,.33" values="12;6;12"/></circle><circle cx="20" cy="12" r="3" fill="#F48E28"><animate id="svgSpinners3DotsBounce1" attributeName="cy" begin="svgSpinners3DotsBounce0.begin+0.2s" calcMode="spline" dur="0.6s" keySplines=".33,.66,.66,1;.33,0,.66,.33" values="12;6;12"/></circle></svg>}
+    {loading && <Chargement/>}
     { data &&
         <ul className='gridCard gridCard--product'>
         {data.map((p) => (

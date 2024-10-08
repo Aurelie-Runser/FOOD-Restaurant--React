45514232d2785b7e71662957e19ecd32d36bec02
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-10-08 13:20:10 +0200

    stylisation du panier

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,13 +1,40 @@
+import { CartContext } from "../../hooks/useCart";
 import GridCard from "../../components/gridCard/GridCard";
+import { useContext } from "react";
+import Bouton from "../../components/bouton/Bouton";
+
+import "./Cart.css"
 
 export default function Cart() {
+  const {cartPrice} = useContext(CartContext)
 
   return <>
-    <h1 className="page--title">My Cart</h1>
-    
-    <GridCard/>
+      <h1 className="page--title">My Cart</h1>
 
-    <hr />
+
+      { cartPrice && <>
+        <GridCard onlyInCart={true}/>
+
+        <hr />
+
+        <div className="cart--infos">
+          <p className="cart--price">Price : <span>$ {cartPrice}</span></p>
+
+          <div className="cart--buttons">
+            <Bouton size="big">Buy</Bouton>
+            <Bouton size="small" rounded>Empty my cart</Bouton>
+          </div>
+        </div>
+       </> }
+
+      { !cartPrice && <div className="cart--empty">
+        <p>Your cart is empty.</p>
+
+        <Bouton size="big" link='/menu'>Add to my cart</Bouton>
+       </div> }
+      
+
+
     
   </>
 }

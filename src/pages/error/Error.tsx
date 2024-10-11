42c6832d41670c26fd6def3ac65121d98cb605bf
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-10-11 18:58:36 +0200

    mise en forme de la page d'erreur

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,9 +1,16 @@
 import { useRouteError } from "react-router-dom"
+import Bouton from "../../components/bouton/Bouton"
+
+import "./Error.css"
 
 export default function Error() {
     const error = useRouteError()
 
-    return <>
+    return <div className="errorPage">
       <h1>Erreur {error?.status}</h1>
-    </>
+
+      <p>{error?.data}</p>
+
+      <Bouton size="big" link="/">Back Home</Bouton>
+    </div>
   }
